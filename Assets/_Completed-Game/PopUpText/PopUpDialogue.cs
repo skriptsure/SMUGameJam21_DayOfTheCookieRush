d471c70dca8373fc0fbd439235f175b0d2accad7
Steven Kiesewetter <steven@elementxcreative.com>
2021-08-21 14:28:08 -0500

    broken commit, do not use

---
+++ Assets/_Completed-Game/PopUpText/PopUpDialogue.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUpDialogue: MonoBehaviour
+{
+    [SerializeField]
+    private Text textUI;
+    [SerializeField]
+    private Image imageUI;
+
+    private float alpha = 0;
+
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - cam.transform.position, Vector3.up);
+        transform.position += Vector3.up * Time.deltaTime;
+        textUI.color = Color.Lerp(Color.white, Color.clear, Math.Max(alpha-1, 0));
+        alpha += Time.deltaTime;
+        if (alpha >= 2)
+            Destroy(gameObject);
+    }
+
+    public void AssignText(string text)
+    {
+        textUI.text = text;
+    }
+
+}
